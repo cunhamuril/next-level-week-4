bb451686d7f998ca34bce543b06e28e74295a222
Murilo Cunha <murilo.sant@hotmail.com>
2021-02-23 22:09:28 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import ExperienceBar from "components/ExperienceBar";
+
+function MyApp() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default MyApp;
